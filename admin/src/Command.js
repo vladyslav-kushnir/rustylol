efdebd79793b7449dd7b2e350aecccfe8a722804
Vladyslav Kushnir <vlad.kushnir92@gmail.com>
2022-07-05 11:34:35 -0700

    Front-end cleanup.

--- admin/src/Command.js
+++ admin/src/Command.js
@@ -3,27 +3,28 @@ import Api from "./api/Api";
 import Variation from "./Variation";
 
 export default function Command(props) {
-    const [variations, setVariations] = useState(Object.values(props.variations).map(x => { return { initial: x.url_pattern, current: x.url_pattern }; }));
+    const [variations, setVariations] = useState(Object.values(props.variations).map(x => { return { initial: x.url_pattern, current: x.url_pattern, isValid: !!x.url_pattern }; }));
     const [name, setName] = useState(props.name);
     const [canSave, setCanSave] = useState(false);
+    const [canDeleteVariations, setCanDeleteVariations] = useState(variations.filter(x => !x.isDeleted).length > 1);
     const [isNew, setIsNew] = useState(props.isNew || false);
 
     let onVariantChange = (idx, value) => {
         const newVariations = [...variations];
-        newVariations[idx] = { ...variations[idx], current: value};
+        newVariations[idx] = { ...variations[idx], current: value, isValid: !!value};
 
         setVariations(newVariations);
     };
 
     let onVariantDelete = (idx) => {
-        if (variations.filter(x => x.current !== undefined).length < 2) {
+        if (!canDeleteVariations) {
             return;
         }
 
         const newVariations = [...variations];
-        if (variations[idx].initial) {
+        if (variations[idx].isNew) {
             // Pre-existing
-            newVariations[idx] = { ...variations[idx], current: undefined };
+            newVariations[idx] = { ...variations[idx], current: '', isDeleted: true, isValid: true };
         }
         else {
             // Newly created
@@ -34,12 +35,14 @@ export default function Command(props) {
     };
 
     let addVariation = () => {
-        setVariations([...variations, { initial: '', current: '' }]);
+        setVariations([...variations, { initial: '', current: '', isNew: true, isValid: false }]);
     };
 
     useEffect(() => {
+        setCanDeleteVariations(variations.filter(x => !x.isDeleted).length > 1);
+
         const nameIsValid = !!name;
-        const variationsAreValid = variations.every(x => x.current === undefined || x.current);
+        const variationsAreValid = variations.every(x => x.isValid);
 
         // Exit early
         if (!nameIsValid || !variationsAreValid) {
@@ -48,7 +51,7 @@ export default function Command(props) {
         }
 
         const nameChanged = props.name != name;
-        const variationsChanged = variations.some(x => x.current === undefined || x.initial != x.current);
+        const variationsChanged = variations.some(x => x.isDeleted || x.initial != x.current);
 
         setCanSave(nameChanged || variationsChanged);
     }, [variations, name]);
@@ -56,7 +59,7 @@ export default function Command(props) {
     let save = async () => {
         if (await Api.upsertCommand({
             name,
-            variations: variations.filter(x => x.current).map(x => x.current)
+            variations: variations.filter(x => !x.isDeleted).map(x => x.current)
         })) {
             setIsNew(false);
             setCanSave(false);
@@ -71,10 +74,10 @@ export default function Command(props) {
 
     return [
         <div key={'data'} style={{ display: 'flex', padding: 5, gap: 10 }}>
-            <div>Name: <input value={name} onChange={change => setName(change.target.value)}/></div>
+            <div>Name: <input value={name} onChange={change => setName(change.target.value)} className={!name ? 'invalid' : undefined}/></div>
             <div>Variations:</div>
             <div>
-                {variations.map((x, idx) => x.current !== undefined ? <Variation key={idx} urlPattern={x.current} onChange={onVariantChange.bind(this, idx)} onDelete={onVariantDelete.bind(this, idx)} canDelete={variations.filter(x => x.current !== undefined).length > 1}/> : undefined).filter(x => x)}
+                {variations.map((x, idx) => !x.isDeleted ? <Variation key={idx} urlPattern={x.current} onChange={onVariantChange.bind(this, idx)} onDelete={onVariantDelete.bind(this, idx)} canDelete={canDeleteVariations} isValid={x.isValid}/> : undefined).filter(x => x)}
                 <div><button onClick={addVariation}>Add variation</button></div>
             </div>
             <div style={{marginLeft: 10, alignSelf: 'center', display: 'flex', gap: 10 }}>
--- admin/src/Variation.js
+++ admin/src/Variation.js
@@ -1,6 +1,6 @@
 export default function Variation(props) {
     return <div>
-        <input value={props.urlPattern} onChange={change => props.onChange(change.target.value)} />
+        <input value={props.urlPattern} onChange={change => props.onChange(change.target.value)} style={{ width: 300 }} className={!props.isValid ? 'invalid' : undefined} />
         {props.canDelete && <button onClick={props.onDelete}>X</button>}
     </div>;
 }
